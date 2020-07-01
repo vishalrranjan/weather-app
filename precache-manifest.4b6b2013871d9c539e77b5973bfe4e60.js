ad1d84eb02308a0410e2666fab1b4e20e8b6646d
vishalrranjan <vishalkr275@gmail.com>
2020-07-01 18:17:16 +0530

    Updates

---
+++ precache-manifest.4b6b2013871d9c539e77b5973bfe4e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d89272c36273a8b94ad0c3f3e2434e5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "bef727d06d2d584c4d6a",
+    "url": "/weather-app/static/css/main.885814f7.chunk.css"
+  },
+  {
+    "revision": "2c4aa51bf863e568b976",
+    "url": "/weather-app/static/js/2.84888ffe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.84888ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef727d06d2d584c4d6a",
+    "url": "/weather-app/static/js/main.530b4bc8.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "32bcc3c888b8b65aac2ab78df9552c4d",
+    "url": "/weather-app/static/media/4.32bcc3c8.jpg"
+  }
+]);
