61bb6dd8d9506d4b9448d0c70189e4156d910ed2
vishalrranjan <vishalkr275@gmail.com>
2020-07-01 18:15:25 +0530

    added project file

---
+++ src/forecast.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect} from 'react';
+import Weather from './weatherData';
+import "./weather.css";
+
+function Forecast (){
+
+// state - 
+  const [countryName, setCountryName] = useState('');
+  const [description, setDescription] = useState('');
+  const [icon, setIcon] = useState('');
+  const [temp, setTemp] = useState('');
+  const [maxTemp, setMaxTemp] = useState('');
+  const [minTemp, setMinTemp] = useState('');
+  const [cityName, setCityName] = useState('')
+  const [sunrise, setSunrise] = useState();
+  const [sunset, setSunset] = useState();
+
+  const [query, setQuery] = useState('delhi');
+  const [search, setSearch] = useState('');
+  // const [load, setLoad] = useState(false);
+
+  // Reach-Hook Lifecycle Methods -
+  
+  useEffect(()=>{
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=11a9b4da876bfdca782f464bb924c855`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data =>{
+          //   setWeatherData(data)
+            setCityName(data.name)
+            setCountryName(data.sys.country)
+            setDescription(data.weather[0].description)
+            setIcon(data.weather[0].icon)
+            setTemp(data.main.temp)
+            setMaxTemp(data.main.temp_max)
+            setMinTemp(data.main.temp_min)
+            setSunrise(data.sys.sunrise)
+            setSunset(data.sys.sunset)
+        }
+      )
+      .catch(error => console.log(error));
+
+  },[query])
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    // console.log(search);
+  };
+
+  const getSearch = (e)=>{
+    e.preventDefault()
+    setQuery(search)
+    setSearch("")
+  }
+
+
+  return(
+    <div>
+        {/* {console.log(this.state.weatherData)} */}
+        
+          <div className="input-box">
+            <input className="search-bar" onChange={updateSearch} type="text" placeholder="Enter Your City" value={search}/>
+            <button className="search-button" onClick={getSearch} title="search your city"><i class="fas fa-search-location"></i></button>
+          </div>
+        
+        <Weather  
+            countryName={countryName} 
+            description={description} 
+            icon={icon} 
+            temp={temp} 
+            cityName={cityName}
+            maxTemp={maxTemp}
+            minTemp={minTemp}
+            sunrise={sunrise}
+            sunset={sunset}
+         />
+      </div>
+  )
+}
+
+export default Forecast;
---
+++ src/weatherData.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./weather.css";
+
+function Weather({countryName,cityName, description, temp,icon, maxTemp, minTemp}) {
+
+    return (
+        <div className="weather-card rounded">
+            <div>
+                <h1 className="primary-font text-center">{cityName}, {countryName}</h1>
+                {/* <p className="numeric-font text-center">Mon, 2:25 PM</p> */}
+                <div className="container row flex-column pl-5">
+                    <div className="row d-flex justify-content-start mt-5">
+                        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon"/>
+                        <h2 className="sub-heading my-auto text-capitalize">{description} </h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-6 ">
+                            <h1 className="large-heading numeric-font">{Math.round(temp)}&deg;</h1>
+                        </div>
+                        <div className="col-6 d-flex justify-content-end align-items-center">
+                            <div>
+                                <p className="temp-font border-bottom m-0 p-0">{Math.round(maxTemp)}&deg;C</p>
+                                <p className="temp-font m-0 p-0">{Math.round(minTemp)}&deg;C</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row d-flex justify-content-center align-items-center mb-2 ">
+                        {/* <div>
+                            <p className="temp-font border-bottom m-0 p-0"><span><i class="fas fa-sun"></i></span>{sunriseTime}</p>
+                            <p className="temp-font m-0 p-0"><span><i class="fas fa-moon"></i></span>{sunsetTime}</p>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
